8bd627c95c457ded4f5a24f3ed933f1e640f1ce8
dennis ronquillo <dennisronquillo@denniss-MacBook-Air.local>
2022-10-27 18:03:18 +0800

    HCC Portal

---
+++ app/src/main/java/com/example/hcc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hcc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Intent dashboard = new Intent(MainActivity.this, Dashboard.class);
+                    startActivity(dashboard);
+                }
+            }
+        });
+    }
+}
